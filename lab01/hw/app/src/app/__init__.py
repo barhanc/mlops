3bb8427b677c5a492a73351b54b7df7a2358f4a6
Bartosz Hanc <bartosz.hanc02@gmail.com>
2025-10-10 22:02:58 +0200

    Initial web server impl

--- lab01/hw/app/src/app/__init__.py
+++ lab01/hw/app/src/app/__init__.py
@@ -1,2 +1,32 @@
-def main() -> None:
-    print("Hello from app!")
+import os
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+from app.inference import SentimentAnalyzer
+
+
+class PredictRequest(BaseModel):
+    text: str
+
+
+class PredictResponse(BaseModel):
+    prediction: str
+
+
+app = FastAPI()
+model = SentimentAnalyzer(
+    sklearn_model_path=os.path.join("models", "classifier.joblib"),
+    sentence_transformer_path=os.path.join("models", "sentence_transformer.model"),
+)
+
+
+@app.get("/")
+def welcome_root():
+    return {"message": "Welcome to the Sentiment Analyzer API"}
+
+
+@app.post("/predict")
+def predict(request: PredictRequest) -> PredictResponse:
+    prediction = model.predict(request.model_dump().get("text"))
+    return PredictResponse(prediction=prediction)
