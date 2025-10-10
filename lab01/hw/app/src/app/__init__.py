ae7eada2e4f2baea1403c93800c2839d39cb72f3
Bartosz Hanc <bartosz.hanc02@gmail.com>
2025-10-10 23:33:52 +0200

    Update inference

--- lab01/hw/app/src/app/__init__.py
+++ lab01/hw/app/src/app/__init__.py
@@ -3,7 +3,7 @@ import os
 from pydantic import BaseModel
 from fastapi import FastAPI
 
-from app.inference import SentimentAnalyzer
+from app.inference import Inference
 
 
 class PredictRequest(BaseModel):
@@ -15,18 +15,12 @@ class PredictResponse(BaseModel):
 
 
 app = FastAPI()
-model = SentimentAnalyzer(
+model = Inference(
     sklearn_model_path=os.path.join("models", "classifier.joblib"),
     sentence_transformer_path=os.path.join("models", "sentence_transformer.model"),
 )
 
 
-@app.get("/")
-def welcome_root():
-    return {"message": "Welcome to the Sentiment Analyzer API"}
-
-
 @app.post("/predict")
 def predict(request: PredictRequest) -> PredictResponse:
-    prediction = model.predict(request.model_dump().get("text"))
-    return PredictResponse(prediction=prediction)
+    return PredictResponse(prediction=model.predict(request.text))
--- lab01/hw/app/src/app/inference.py
+++ lab01/hw/app/src/app/inference.py
@@ -1,35 +1,57 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
-
+import torch
 import joblib
 
 from cleantext import clean
 from sentence_transformers import SentenceTransformer
 
 
-def load_sklearn_model(path: str):
+def _load_sklearn_model(path: str):
     with open(path, "rb") as file:
         model = joblib.load(file)
     return model
 
 
-def load_sentence_transformer(path: str) -> SentenceTransformer:
+def _load_sentence_transformer(path: str) -> SentenceTransformer:
     return SentenceTransformer(model_name_or_path=path)
 
 
-class SentimentAnalyzer:
+def _get_device():
+    if torch.cuda.is_available():
+        return "cuda"
+    if torch.mps.is_available():
+        return "mps"
+    return "cpu"
+
+
+class Inference:
     sentiment_map = {0: "negative", 1: "neutral", 2: "positive"}
 
     def __init__(self, sklearn_model_path: str, sentence_transformer_path: str):
-        self._sklearn_model = load_sklearn_model(sklearn_model_path)
-        self._sentence_transformer = load_sentence_transformer(sentence_transformer_path)
+        self._sklearn_model = _load_sklearn_model(sklearn_model_path)
+        self._sentence_transformer = _load_sentence_transformer(sentence_transformer_path)
+        self._device = _get_device()
+
+    def _validate_input(self, text: str) -> str:
+        if not isinstance(text, str):
+            raise TypeError("Expected input to be a string")
+        if len(text.strip()) < 3:
+            raise ValueError("Text is too short to analyze")
+
+        return text
+
+    def _preprocess(self, text: str) -> str:
+        text = self._validate_input(text)
+        text = clean(text)
+        return text
 
     def predict(self, text: str) -> str:
-        text = clean(text)
+        try:
+            text = self._preprocess(text)
+            embeddings = self._sentence_transformer.encode([text], device=self._device)
+            sentiment = self._sklearn_model.predict(embeddings)
+            sentiment = sentiment[0]
 
-        embeddings = self._sentence_transformer.encode([text])
-        sentiment = self._sklearn_model.predict(embeddings)
-        sentiment = sentiment[0]
+            return self.sentiment_map[sentiment]
 
-        return self.sentiment_map[sentiment]
+        except Exception as e:
+            return f"ERROR: {e}"
