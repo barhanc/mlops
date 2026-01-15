6771574ccc8ce85b18a70077ae456bbadc37a4e3
Bartosz Hanc <bartosz.hanc02@gmail.com>
2026-01-15 23:01:47 +0100

    Add example taskflow pipeline

---
+++ lab12/dags/02_taskflow_pipeline.py
@@ -0,0 +1,39 @@
+import requests
+import pandas as pd
+
+from airflow.sdk import dag, task
+
+
+@dag()
+def weather_data_classes_api_v2():
+    @task()
+    def get_data() -> dict:
+        print("Fetching data from API")
+
+        # New York temperature in 2025
+        url = "https://archive-api.open-meteo.com/v1/archive?latitude=40.7143&longitude=-74.006&start_date=2025-01-01&end_date=2025-12-31&hourly=temperature_2m&timezone=auto"
+
+        resp = requests.get(url)
+        resp.raise_for_status()
+
+        data = resp.json()
+        data = {"time": data["hourly"]["time"], "temperature": data["hourly"]["temperature_2m"]}
+        return data
+
+    @task()
+    def transform(data: dict) -> pd.DataFrame:
+        df = pd.DataFrame(data)
+        df["temperature"] = df["temperature"].clip(lower=-20, upper=50)
+        return df
+
+    @task()
+    def load(df: pd.DataFrame) -> None:
+        print("Saving the data")
+        df.to_csv("data.csv", index=False)
+
+    data = get_data()
+    df = transform(data)  # type:ignore
+    load(df)  # type:ignore
+
+
+weather_data_classes_api_v2()
