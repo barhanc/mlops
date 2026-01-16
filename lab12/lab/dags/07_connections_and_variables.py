edab7f2d45c0ac55fe87b70fbdf4fc8c3d768ee0
Bartosz Hanc <bartosz.hanc02@gmail.com>
2026-01-16 22:02:27 +0100

    Add exercise 5

---
+++ lab12/lab/dags/07_connections_and_variables.py
@@ -0,0 +1,44 @@
+import datetime
+
+from airflow.sdk import dag, task, Variable
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+POSTGRES_CONN_ID = "postgres_storage"
+
+
+@dag(schedule=datetime.timedelta(minutes=1))
+def scheduling_dataset_gathering_with_venvs_and_connections():
+
+    @task.virtualenv(
+        requirements=["twelvedata", "pendulum", "lazy-object-proxy", "cloudpickle"],
+        serializer="cloudpickle",
+        python_version="3",
+        system_site_packages=False,
+    )
+    def get_data(*, apikey: str, logical_date) -> dict:
+        from twelvedata import TDClient
+
+        td = TDClient(apikey)
+        ts = td.exchange_rate(symbol="USD/EUR", date=logical_date.isoformat())
+        return ts.as_json()  # type: ignore
+
+    @task
+    def save_data(data: dict) -> None:
+        print("Saving the data")
+
+        if not data:
+            raise ValueError("No data received")
+
+        pg_hook = PostgresHook(postgres_conn_id="postgres_storage")
+        pg_hook.insert_rows(
+            table="exchange_rates",
+            rows=[(data["symbol"], data["rate"])],
+            target_fields=["symbol", "rate"],
+        )
+
+    data = get_data(apikey=Variable.get("TWELVEDATA_API_KEY"))  # type: ignore
+    save_data(data)
+
+
+scheduling_dataset_gathering_with_venvs_and_connections()
---
+++ lab12/lab/init-db/init.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS exchange_rates;
+
+CREATE TABLE exchange_rates ("symbol" TEXT, "rate" DECIMAL);
